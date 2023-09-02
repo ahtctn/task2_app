a0cede08aa4b00c180b62ca797bd1ecbd74b374b
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-09-02 16:13:03 +0300

    timerView

--- task2_app/View/TimerView/TimerView.swift
+++ task2_app/View/TimerView/TimerView.swift
@@ -8,10 +8,64 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State private var targetDate = Date()
+    @State private var remainingTime: TimeInterval = 0
+    @State private var timer: Timer? = nil
+    
     var body: some View {
-        Text("TimerView")
+        VStack {
+            Text("Geri Sayım")
+                .font(.largeTitle)
+            
+            DatePicker("Hedef Tarih Seçin", selection: $targetDate, in: Date()..., displayedComponents: .hourAndMinute)
+                .datePickerStyle(.wheel)
+                .labelsHidden()
+            
+            
+            
+            Text("Kalan Süre:")
+                .font(.headline)
+            
+            Text(timeFormatted(remainingTime))
+                .font(.largeTitle)
+                .onAppear {
+                    startTimer()
+                }
+            
+            Button("Sıfırla") {
+                stopTimer()
+                remainingTime = 0
+            }
+        }
+        .padding()
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            let now = Date()
+            remainingTime = targetDate.timeIntervalSince(now)
+            if remainingTime <= 0 {
+                stopTimer()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func timeFormatted(_ seconds: TimeInterval) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        return formatter.string(from: seconds) ?? "00:00:00"
     }
 }
+
+
+
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
